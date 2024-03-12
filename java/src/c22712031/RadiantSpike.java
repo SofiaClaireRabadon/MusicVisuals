e8f43eac07813d92d6f60524c6eb27c787fe4fb8
SofiaClaireRabadon <C22712031@mytudublin.ie>
2024-03-12 22:32:35 +0000

    updated-2

--- java/src/c22712031/RadiantSpike.java
+++ java/src/c22712031/RadiantSpike.java
@@ -6,6 +6,7 @@ public class RadiantSpike {
 
     SofiasVisual ssv;
     float height = 0;
+    float width = 0;
     float rot = 0;
 
     public RadiantSpike(SofiasVisual ssv)
@@ -28,6 +29,40 @@ public class RadiantSpike {
             ssv.stroke(colour, 255, 255);
             ssv.strokeWeight(1);       
             ssv.box(300 * ssv.smoothedAmplitude);
+            ssv.popMatrix();
+            rot += 10;    
+        } 
+
+        for(float b2 = 0; b2 < 100; b2 ++)
+        {
+            ssv.camera(0, 0, 200, 0, 0, 0, 1, 0, 0);
+            ssv.pushMatrix();
+            ssv.translate(500, height * 0.9f, -999);
+            ssv.rotateX(PApplet.radians(rot)); // spins right and left
+            //ssv.rotateY(PApplet.radians(rot)); // spins forward and backward
+            //ssv.rotateZ(PApplet.radians(rot)); // spins around like a circle
+            ssv.fill(178, 0, 0);
+            float colour = PApplet.map(b2, 0, 9.5f, b2, 65);
+            ssv.stroke(colour, 255, 255);
+            ssv.strokeWeight(1);       
+            ssv.box(900 * ssv.smoothedAmplitude);
+            ssv.popMatrix();
+            rot += 10;    
+        } 
+
+        for(float b3 = 0; b3 < 100; b3 ++)
+        {
+            ssv.camera(0, 0, 200, 0, 0, 0, 1, 0, 0);
+            ssv.pushMatrix();
+            ssv.translate(-500, height * 0.9f, -999);
+            ssv.rotateX(PApplet.radians(rot)); // spins right and left
+            //ssv.rotateY(PApplet.radians(rot)); // spins forward and backward
+            //ssv.rotateZ(PApplet.radians(rot)); // spins around like a circle
+            ssv.fill(178, 0, 0);
+            float colour = PApplet.map(b3, 0, 9.5f, b3, 65);
+            ssv.stroke(colour, 255, 255);
+            ssv.strokeWeight(1);       
+            ssv.box(900 * ssv.smoothedAmplitude);
             ssv.popMatrix();
             rot += 10;    
         } 
